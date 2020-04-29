df5396b7b6f02cf6552990dce3f7f53f5caf54a3
AntMan <xanthonyalvarezx@gmail.com>
2020-04-29 15:18:06 -0400

    beautified all of my files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,19 @@
 //  team one
- 
- let shotsOne = document.querySelector('#teamone-shoot-button')
- let teamOneShots = document.querySelector('#teamone-numshots')
- let tmOneGoals = document.querySelector('#teamone-numgoals')
-shotsOne.addEventListener('click', function(){
+
+let shotsOne = document.querySelector('#teamone-shoot-button')
+let teamOneShots = document.querySelector('#teamone-numshots')
+let tmOneGoals = document.querySelector('#teamone-numgoals')
+shotsOne.addEventListener('click', function () {
 
     let teamOneNum = parseInt(teamOneShots.innerText) + 1
     teamOneShots.innerText = teamOneNum
 
 
-let tmOneScore = parseInt(tmOneGoals.innerText) + Math.floor(Math.random() * 2)
-tmOneGoals.innerText = tmOneScore
+    let tmOneScore = parseInt(tmOneGoals.innerText) + Math.floor(Math.random() * 2)
+    tmOneGoals.innerText = tmOneScore
 
- 
-    
+
+
 
 })
 // team two
@@ -22,45 +22,34 @@ let shotsTwo = document.querySelector('#teamtwo-shoot-button')
 const teamTwoShots = document.querySelector('#teamtwo-numshots')
 const tmTwoGoals = document.querySelector('#teamtwo-numgoals')
 
-shotsTwo.addEventListener('click', function(){
+shotsTwo.addEventListener('click', function () {
 
-   let teamTwoNum = parseInt(teamTwoShots.innerText) + 1
-   teamTwoShots.innerText = teamTwoNum
+    let teamTwoNum = parseInt(teamTwoShots.innerText) + 1
+    teamTwoShots.innerText = teamTwoNum
 
-let tmTwoScore = parseInt(tmTwoGoals.innerText) + Math.floor(Math.random() * 2)
-tmTwoGoals.innerText = tmTwoScore
+    let tmTwoScore = parseInt(tmTwoGoals.innerText) + Math.floor(Math.random() * 2)
+    tmTwoGoals.innerText = tmTwoScore
 
 })
 // reset button
 
 let reset = document.querySelector('#reset-button')
 let resetNum = document.querySelector('#num-resets')
-reset.addEventListener('click', function(){
-let startOver = parseInt(resetNum.innerText) + 1
-resetNum.innerText = startOver
+reset.addEventListener('click', function () {
+    let startOver = parseInt(resetNum.innerText) + 1
+    resetNum.innerText = startOver
 
-if (tmOneGoals.innerText > tmTwoGoals.innerText){
-    alert('Team one wins!!!')
-}
+    if (tmOneGoals.innerText > tmTwoGoals.innerText) {
+        alert('Team one wins!!!')
+    } else {
+        alert('Team two wins!!!!')
+    }
 
-else{
-    alert('Team two wins!!!!')
-}
-
-teamOneShots.innerText = 0
-tmOneGoals.innerText = 0
-teamTwoShots.innerText = 0
-tmTwoGoals.innerText = 0
+    teamOneShots.innerText = 0
+    tmOneGoals.innerText = 0
+    teamTwoShots.innerText = 0
+    tmTwoGoals.innerText = 0
 })
 
 let shot = new Audio()
-shot.src ='Back+Board.mp3'
-
-
-
-
-
-
-
-
-
+shot.src = 'Back+Board.mp3'
