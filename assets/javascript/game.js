779162fb33a7bd2c8b16f9da09e90a36ecebaa6a
AntMan <xanthonyalvarezx@gmail.com>
2020-04-29 10:51:56 -0400

    got the reset and winner message to work upon reset 'click'

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,14 @@
+//  team one
+ 
  let shotsOne = document.querySelector('#teamone-shoot-button')
  let teamOneShots = document.querySelector('#teamone-numshots')
-
+ let tmOneGoals = document.querySelector('#teamone-numgoals')
 shotsOne.addEventListener('click', function(){
 
     let teamOneNum = parseInt(teamOneShots.innerText) + 1
     teamOneShots.innerText = teamOneNum
 
-let tmOneGoals = document.querySelector('#teamone-numgoals')
+
 let tmOneScore = parseInt(tmOneGoals.innerText) + Math.floor(Math.random() * 2)
 tmOneGoals.innerText = tmOneScore
 
@@ -14,28 +16,47 @@ tmOneGoals.innerText = tmOneScore
     
 
 })
-
+// team two
 
 let shotsTwo = document.querySelector('#teamtwo-shoot-button')
-let teamTwoShots = document.querySelector('#teamtwo-numshots')
+const teamTwoShots = document.querySelector('#teamtwo-numshots')
+const tmTwoGoals = document.querySelector('#teamtwo-numgoals')
 
 shotsTwo.addEventListener('click', function(){
 
    let teamTwoNum = parseInt(teamTwoShots.innerText) + 1
    teamTwoShots.innerText = teamTwoNum
 
-   let tmTwoGoals = document.querySelector('#teamtwo-numgoals')
 let tmTwoScore = parseInt(tmTwoGoals.innerText) + Math.floor(Math.random() * 2)
 tmTwoGoals.innerText = tmTwoScore
 
 })
-
+// reset button
 
 let reset = document.querySelector('#reset-button')
 let resetNum = document.querySelector('#num-resets')
 reset.addEventListener('click', function(){
 let startOver = parseInt(resetNum.innerText) + 1
 resetNum.innerText = startOver
+
+if (tmOneGoals.innerText > tmTwoGoals.innerText){
+    document.write('Team one wins!!!')
+}
+
+else{
+    document.write('Team two wins!!!!')
+}
+
+teamOneShots.innerText = 0
+tmOneGoals.innerText = 0
+teamTwoShots.innerText = 0
+tmTwoGoals.innerText = 0
+
+
+
+
+
+
 })
 
 
